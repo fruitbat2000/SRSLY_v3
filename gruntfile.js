f2d7aa43aa13b229699ea345d0b20ecc2d18517a
Vix <vicki.keeley@gmail.com>
2013-03-30 12:41:31 +0000

    node and grunt files

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //jshint config 
+    jshint: {
+      files: ['js/*.js, js/components/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console:true
+        }
+      }
+    },
+
+    //stylus config 
+    stylus: {
+      compile: {
+        options: {
+          compress:false,
+          paths: ['css/css_stylus', 'css_stylus/modules']
+        },
+        files: {
+          'css/index.css': 'css/css_stylus/index.styl'
+        }
+      }
+    },
+
+    //watch
+    watch: {
+      styles: {
+        files: ['css/**/*.styl'],
+        tasks: ['stylus']
+      },
+      scripts: {
+        files: ['js/**/*.js'],
+        tasks: ['jshint']
+      }
+
+    }
+  });
+
+  //load modules
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //default task 
+  grunt.registerTask('default', ['jshint', 'stylus']);
+
+  //other tasks 
+  grunt.registerTask('styles', 'stylus');
+  grunt.registerTask('test', 'jshint');
+};
